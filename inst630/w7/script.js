54451d1a7b270be10c3ec5e0e4a173ffaea26a82
erinlcampbell <156254627+erinlcampbell@users.noreply.github.com>
2025-10-15 10:30:10 -0400

    Correct class assignment based on inspection results
    Fix class assignment for restaurant compliance status.

--- inst630/w7/script.js
+++ inst630/w7/script.js
@@ -131,9 +131,17 @@ document.addEventListener('DOMContentLoaded', function() {
         
         // YOUR CODE HERE:
         restaurants.forEach(function(restaurant, index) { 
+            if ((restaurant.properties.inspection_results).includes("non"){
+                const class = "non-compliant";
+            } else {
+                const class = "compliant";
+            }
+            
             cardGrid.innerHTML += `<div class="restaurant-card">
                 <div class="card-name">${restaurant.properties.name}</div>
                 <div class="card-location">${restaurant.properties.city}</div>
+                <div class="card-status ${class}">${restaurant.properties.inspection_results}</div>
+                
             </div>`
             
         })
