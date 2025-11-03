f5b8772a0d4e962ba54fa4de5d3ead6d2309ea02
erinlcampbell <156254627+erinlcampbell@users.noreply.github.com>
2025-11-03 11:58:38 -0500

    Enhance GSAP animation properties for restaurant cards

--- inst630/w8/script.js
+++ inst630/w8/script.js
@@ -205,13 +205,14 @@ function animateMyCards() {
             // FROM state (starting point)
             {
                 /* TODO: Starting properties - opacity, scale, y position? */
+                opacity: 0, scale: 0.5, y: 200
             },
             // TO state (ending point)  
             {
                 /* TODO: Ending properties - make them fully visible and normal size */
-                duration: '0.2'
-                // stagger: /* TODO: Delay between each card? 0.1 seconds? */,
-                // ease: /* TODO: What kind of easing? "bounce.out"? */
+                duration: '0.2',
+                stagger: 0.1 /* TODO: Delay between each card? 0.1 seconds? */,
+                ease: "bounce.out"/* TODO: What kind of easing? "bounce.out"? */
             }
         );
         
