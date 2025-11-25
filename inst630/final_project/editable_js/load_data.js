6dcb8bd86232edebf6dcde05c86c6e8b1be80668
erinlcampbell <elcampbell@wm.edu>
2025-11-25 11:35:43 -0500

    fish added! and data organized

--- inst630/final_project/editable_js/load_data.js
+++ inst630/final_project/editable_js/load_data.js
@@ -4,21 +4,90 @@
 /**
  * Load data from API - Students replace with their chosen endpoint
  */
+
+function getSeasons(animalData){
+  let spring = [];
+  let summer = [];
+  let fall = [];
+  let winter = [];
+
+  animalData.forEach(
+    (animal) =>
+          {
+              if (animal["NH Mar"] !== "NA" || animal["NH Apr"] !== "NA" || animal["NH May"] !== "NA"){
+                    spring.push(animal);
+              }
+              if (animal["NH Jun"] !== "NA" || animal["NH Jul"] !== "NA" || animal["NH Aug"] !== "NA"){
+                    summer.push(animal);
+              }
+              if (animal["NH Sep"] !== "NA" || animal["NH Oct"] !== "NA" || animal["NH Nov"] !== "NA"){
+                    fall.push(animal);
+              }
+              if (animal["NH Dec"] !== "NA" || animal["NH Jan"] !== "NA" || animal["NH Feb"] !== "NA"){
+                    winter.push(animal);
+              }
+          }
+  );
+
+  const seasons = {
+    "spring": spring,
+    "summer": summer,
+    "fall": fall,
+    "winter": winter
+  };
+  return seasons;
+}
+
 async function loadData() {
   try {
 
-    const apiUrl = `http://acnhapi.com/v1/fish`;
-    const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(apiUrl)}`;
+    const responseFish = await fetch('fish.json');
+    let dataFish = "";
+    if (responseFish.ok){
+        dataFish = await responseFish.json();
+        console.log(dataFish);
+        
 
-    const response = await fetch (proxyUrl)
-    const data = await response.json();
-    console.log("data loaded", data);
+    } else {
+        alert("HTTP-Error: " + responseFish.status);
+    }
 
-    return data;
+    const responseBugs = await fetch('insects.json');
+    let dataBugs = "";
+    if (responseBugs.ok){
+        dataBugs = await responseBugs.json();
+        console.log(dataBugs);
+        
+
+    } else {
+        alert("HTTP-Error: " + responseBugs.status);
+    }
+
+    const responseCreatures = await fetch('creatures.json');
+    let dataCreatures = "";
+    if (responseCreatures.ok){
+        dataCreatures = await responseCreatures.json();
+        console.log(dataCreatures);
+        
+
+    } else {
+        alert("HTTP-Error: " + responseCreatures.status);
+    }
+
+    const fishSeasons = getSeasons(dataFish);
+    const bugsSeasons = getSeasons(dataBugs);
+    const creaturesSeasons = getSeasons(dataCreatures);
+
+    const data = {
+      "fish": fishSeasons,
+      "bugs": bugsSeasons,
+      "creatures": creaturesSeasons
+    }
+    console.log(data);
+    console.log(data.bugs.summer);
     
-    // Simulate API delay
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
-    // return mockRestaurantData;
+    return data;
+
   } catch (error) {
     console.error("Failed to load data:", error);
     throw new Error("Could not load data from API");
--- inst630/final_project/editable_js/template_fish.js
+++ inst630/final_project/editable_js/template_fish.js
@@ -4,22 +4,96 @@
  * Display data as browsable cards - good for comparing individual items
  */
 function showFish(data) {
-  const cardHTML = data
+  const fishSpringHTML = data.fish.spring
     .map(
        /*html*/ 
-      (camera) => `
-                <div class="restaurant-card">
-                    test
+      (fish) => `
+                <div class="animalObject">
+                  <img class="animalImage" src="${fish.Image_Url}" alt="">
+                  <div class="description">
+                    <h3 class="animalName">${fish.Name}</h3>
+                    <h5 class="bells">Bells: ${fish.Sell}</h5>
+                    <h5 class="location">Location: ${fish["Where/How"]}</h5>
+                    <h5 class="shadow">Shadow: ${fish.Shadow}</h5>
+                    <h5 class="difficulty">Catch Difficulty: ${fish["Catch Difficulty"]}
+                  </div>
+                </div>
+            `
+    )
+    .join("");
+
+    const fishSummerHTML = data.fish.summer
+    .map(
+       /*html*/ 
+      (fish) => `
+                <div class="animalObject">
+                  <img class="animalImage" src="${fish.Image_Url}" alt="">
+                  <div class="description">
+                    <h3 class="animalName">${fish.Name}</h3>
+                    <h5 class="bells">Bells: ${fish.Sell}</h5>
+                    <h5 class="location">Location: ${fish["Where/How"]}</h5>
+                    <h5 class="shadow">Shadow: ${fish.Shadow}</h5>
+                    <h5 class="difficulty">Catch Difficulty: ${fish["Catch Difficulty"]}
+                  </div>
+                </div>
+            `
+    )
+    .join("");
+
+    const fishFallHTML = data.fish.fall
+    .map(
+       /*html*/ 
+      (fish) => `
+                <div class="animalObject">
+                  <img class="animalImage" src="${fish.Image_Url}" alt="">
+                  <div class="description">
+                    <h3 class="animalName">${fish.Name}</h3>
+                    <h5 class="bells">Bells: ${fish.Sell}</h5>
+                    <h5 class="location">Location: ${fish["Where/How"]}</h5>
+                    <h5 class="shadow">Shadow: ${fish.Shadow}</h5>
+                    <h5 class="difficulty">Catch Difficulty: ${fish["Catch Difficulty"]}
+                  </div>
+                </div>
+            `
+    )
+    .join("");
+
+    const fishWinterHTML = data.fish.winter
+    .map(
+       /*html*/ 
+      (fish) => `
+                <div class="animalObject">
+                  <img class="animalImage" src="${fish.Image_Url}" alt="">
+                  <div class="description">
+                    <h3 class="animalName">${fish.Name}</h3>
+                    <h5 class="bells">Bells: ${fish.Sell}</h5>
+                    <h5 class="location">Location: ${fish["Where/How"]}</h5>
+                    <h5 class="shadow">Shadow: ${fish.Shadow}</h5>
+                    <h5 class="difficulty">Catch Difficulty: ${fish["Catch Difficulty"]}</h5>
+                  </div>
                 </div>
             `
     )
     .join("");
      /*html*/ 
   return `
-                <h2 class="view-title"><img class="view_image" src="https://i.pinimg.com/736x/28/ae/e5/28aee52766b152950e62c543dc867df0.jpg" alt="mario"> Card View</h2>
-                <div class="card-grid">
-                    <img src="https://media.tenor.com/KXWh0Y-P9iEAAAAe/lakitu-mario-kart.png" alt="lakitu">
-                    ${cardHTML}
+                <div class="scrollerContainer">
+                  <div class="season spring">
+                    <h2>Spring</h2>
+                    ${fishSpringHTML}
+                  </div>
+                  <div class="season summer">
+                    <h2>Summer</h2>
+                    ${fishSummerHTML}
+                  </div>
+                  <div class="season fall">
+                    <h2>Fall</h2>
+                    ${fishFallHTML}
+                  </div>
+                  <div class="season winter">
+                    <h2>Winter</h2>
+                    ${fishWinterHTML}
+                  </div>
                 </div>
             `;
 }
